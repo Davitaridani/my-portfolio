c8c03caa11e8da82d91aea47ccf461ee9f8728f9
Davitaridani <davitaridani@gmail.com>
2023-04-03 20:36:50 +0700

    responsive mobile section about and herro banner

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -23,22 +23,22 @@ const About = () => {
 	}
 
 	return (
-		<section id="about">
+		<section className='pt-8 md:pt-12' id="about">
 			<div className="container overflow-hidden">
-				<h1 className="text-center mb-8 font-bold text-3xl text-headingColor">About Me</h1>
-				<div className="flex">
+				<h1 className="text-center mb-8 font-bold text-[28px] md:text-3xl text-headingColor">About Me</h1>
+				<div className="block md:flex">
 					<div className="w-full basis-1/2">
-						<h3 className='text-3xl font-bold text-headingColor leading-8 mb-6' data-aos="fade-right"
+						<h3 className='text-[24px] md:text-3xl font-bold text-headingColor leading-8 mb-4 md:mb-6' data-aos="fade-right"
 							data-aos-duration="2000">Qualification</h3>
-						<div className="h-[250px] overflow-y-auto p-2 scrollbar-hide">
+						<div className="h-[250px] overflow-y-auto p-2 scrollbar-hide mb-8 md:mb-0">
 							<ol className='relative border-l border-headingColor' data-aos="fade-right"
 								data-aos-duration="2500" data-aos-delay="250">
 								{qualification.map((item, index) => (
-									<li className='mb-5 ml-4' key={index} >
+									<li className='mb-4 md:mb-5 ml-4' key={index} >
 										<div className='absolute w-3 h-3 rounded-full mt-1 -left-1.5 border-2 bg-primaryColor' >
 										</div>
 										<h3 className="text-headingColor font-semibold text-sm">{item.year}</h3>
-										<p className="text-lg font-bold leading-5 -headingColor">{item.school}</p>
+										<p className="text-base md:text-lg font-bold leading-5 -headingColor">{item.school}</p>
 										<span className="font-medium text-[15px] text-gray-500">{item.text}</span>
 									</li>
 								))}
@@ -49,12 +49,12 @@ const About = () => {
 					<div className="basis-1/2">
 						<div className="mb-3" data-aos="fade-left"
 							data-aos-duration="2000">
-							<h3 className="text-3xl font-bold text-headingColor leading-8">Ahmat Davit</h3>
+							<h3 className="text-[24px] md:text-3xl font-bold text-headingColor leading-6 md:leading-8">Ahmat Davit</h3>
 							<p className="text-xs text-gray-400" data-aos="fade-left"
 								data-aos-duration="2500">Developer & Photographer</p>
 						</div>
-						<p className="font-medium text-headingColor font-sans mb-7" data-aos="fade-left"
-							data-aos-duration="2500" data-aos-delay="250">Hi, introduce my name is Ahmat Davit Ari Dani. I am a Front End Developer and Photographer. I graduated from the Jember State Polytechnic campus in 2022 and now I am participating in a fullstack developer bootcamp at Binar Academy from February to August 2023. I started coding at the beginning of college and I have experience in developing web applications such as Html, Css, Scss, Js, Bootstrap , Tailwind, ReactJs, Web Animation and other web tools. Apart from that, I also have freelance photography work. So, here I can help you build your web interface and design your project.</p>
+						<p className="font-medium text-[15px] md:text-base  text-headingColor font-sans mb-7" data-aos="fade-left"
+							data-aos-duration="2500" data-aos-delay="250">Hii, introduce my name is Ahmat Davit Ari Dani. I am a Front End Developer and Photographer. I graduated from the Jember State Polytechnic campus in 2022 and now I am participating in a fullstack developer bootcamp at Binar Academy from February to August 2023. I started coding at the beginning of college and I have experience in developing web applications such as Html, Css, Scss, Js, Bootstrap, Tailwind, React, Web Animation and other web tools. Apart from that, I also have freelance photography work. So, here I can help you build your web interface and design your project.</p>
 						<a onClick={handleClick} href='#contact' className='flex items-center gap-1 bg-primaryColor  max-w-fit text-[15px] text-white font-medium rounded-[5px] py-2 px-3 hover:bg-smallTextColor hover:text-white duration-200 ease-in'><span><BsSend /></span> Contact me</a>
 						<div className="flex items-center gap-3">
 						</div>
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -15,26 +15,26 @@ const Hero = () => {
 	}, []);
 
 	return (
-		<section className="pt-0" id="home">
-			<div className="container pt-14">
-				<div className="md:flex items-center justify-between sm:flex-col md:flex-row">
+		<section className="lg:pt-0 pt-10 pb-0 md:pb-12" id="home">
+			<div className="container pt-14 ">
+				<div className="md:flex  items-center justify-between sm:flex-col md:flex-row ">
 					<div className="w-full md:basis-1/2">
-						<h5 className="font-bold text-headingColor  text-[40px] leading-[35px] sm:leading-[45px] mt-5"
+						<h5 className="font-bold text-headingColor text-[35px] md:text-[40px] leading-[43px] sm:leading-[45px] mt-5"
 							data-aos="fade-right"
 							data-aos-duration="2000"
 						>I'm Ahmat Davit <br />a <span className='text-primaryColor' data-aos="fade-right"
 							data-aos-duration="2000" data-aos-delay="300">Front End Developer</span></h5>
-						<div className="flex items-center gap-6 mt-7">
-							<a href="mailto:davitaridani@gmail.com" data-aos="fade-up" data-aos-duration="2000"><span className='flex items-center bg-primaryColor text-white font-medium gap-1 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px]'><HiOutlineMail /> Hire Me</span>
+						<div className="flex items-center gap-4 md:gap-6 mt-7">
+							<a href="mailto:davitaridani@gmail.com" data-aos="fade-up" data-aos-duration="2000"><span className='flex items-center bg-primaryColor text-white font-medium gap-1 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px] text-[15px] md:text-base'><HiOutlineMail /> Hire Me</span>
 							</a>
-							<a href={Imgownload} download className="flex items-center text-smallTextColor font-semibold text-base py-2 px-4 rounded-[8px] border border-solid border-smallTextColor gap-1 hover:border-primaryColor hover:bg-primaryColor hover:text-white ease-in duration-300" data-aos="fade-up" data-aos-duration="2000">  <BsCloudArrowDownFill /> Download CV</a>
+							<a href={Imgownload} download className="flex items-center text-smallTextColor font-semibold py-2 px-4 rounded-[8px] border border-solid border-smallTextColor gap-1 hover:border-primaryColor hover:bg-primaryColor hover:text-white ease-in duration-300 text-[15px] md:text-base" data-aos="fade-up" data-aos-duration="2000"> <BsCloudArrowDownFill /> Download CV</a>
 						</div>
-						<p className='flex gap-1 text-headingColor font-medium mt-12 text-[15px] leading-7 sm:pl-14 sm:pr-10' data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300">
+						<p className='flex gap-1 text-headingColor font-medium mt-9 md:mt-12 text-[15px] leading-7 sm:pl-14 sm:pr-10' data-aos="fade-right" data-aos-duration="2000" data-aos-delay="300">
 							<span className='pt-2'> <TbApps /> </span>
-							I can help you build beautiful and attractive web interface <br /> for your projects. So let's talk!
+							I can help you build beautiful and attractive web interface <br className="hidden md:block" /> for your projects. So let's talk!
 						</p>
-						<div className="flex items-center gap-4 mt-14">
-							<span className="text-smallTextColor text-sm font-semibold">Follow Me : </span>
+						<div className="flex items-center gap-[10px] md:gap-4 mt-10 md:mt-14">
+							<span className="text-smallTextColor text-[15px] font-semibold">Follow Me : </span>
 							{iconHero.map((item, index) => (
 								<span key={index} className='border-solid border-2 border-primaryColor flex items-center justify-center h-[30px] w-[30px] rounded-full hover:bg-primaryColor duration-300 ease-in cursor-pointer group' data-aos="zoom-in" data-aos-duration="3000" data-aos-delay={item.delayAos}>
 									<a href={item.link} target="_blank" rel="noreferrer" className="text-smallTextColor duration-300 ease-in group-hover:text-white text-base font-semibold ">{item.icon}</a>
@@ -45,11 +45,11 @@ const Hero = () => {
 
 					<div className="basis-1/3 mt-10 sm:mt-0">
 						<div className="img-profile flex items-center justify-center overflow-hidden" data-aos="zoom-in-down" data-aos-duration="2500" data-aos-delay="300">
-							<img src={imgHero} alt="" className="rounded-tl-[125px] rounded-tr-[125px] rounded-bl-[25px] rounded-br-[25px] mt-8 animate-shake" />
+							<img src={imgHero} alt="" className="rounded-tl-[125px] rounded-tr-[125px] rounded-bl-[25px] rounded-br-[25px] mt-8 animate-shake max-w-[90%]" />
 						</div>
 					</div>
 
-					<div className="md:basis-1/12 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
+					<div className="md:basis-1/12 basis-0 flex justify-between text-center flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
 						<div className="mb-10"></div>
 					</div>
 				</div>
